5ce55d5282bd9c2c1d5189dfd1814bb7cc7d27ea
tomoaki <tomoaki@localhost.localdomain>
2014-02-22 02:46:18 +0900

    set my App ID and home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ActionController::Base
   protect_from_forgery
   
    def index   
-   	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
+   	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + ':3000/home/callback')
 		@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream") 	
 		puts session.to_s + "<<< session"
 
