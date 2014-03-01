291c1534fd783df5260d55e44594a6b4458ecf0d
tomoaki <tomoaki@localhost.localdomain>
2014-03-01 16:46:18 +0900

    .gitignoreを反映

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
-APP_ID= '219868431409649' # please change!
-APP_SECRET= '7e5699f155df01d8e52b35c01dccd627' # please change!
-SITE_URL = 'http://localhost:3000'
+APP_ID= '207139866146210' # please change!
+APP_SECRET= '22785a528a652808aac341a43fb2a4b3' # please change!
+SITE_URL = 'http://www.tomoimapp.com'
