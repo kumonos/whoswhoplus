9ed15bf2ebf7c34f3db75f492130ed8f810066f9
youcune <i@youcune.com>
2014-03-19 08:46:24 +0900

    テストデータ仮作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'factory_girl'
+Dir[Rails.root.join('spec/factories/*.rb')].each { |f| require f }
+
+# Profile作成
+p1 = FactoryGirl.create(:profile)
+p2 = FactoryGirl.create(:profile)
+p3 = FactoryGirl.create(:profile)
+p4 = FactoryGirl.create(:profile)
+p5 = FactoryGirl.create(:profile)
+
+# 適当に友達関係を結ぶ
+# 1が認証ユーザ
+# 1と2, 1と3が友達
+# 2と4, 2と5が友達
+# 3と4が友達
+FactoryGirl.create(:relation, friend_friend: p4.fb_id, friend_mutual: p2.fb_id, profile: p1)
+FactoryGirl.create(:relation, friend_friend: p5.fb_id, friend_mutual: p2.fb_id, profile: p1)
+FactoryGirl.create(:relation, friend_friend: p4.fb_id, friend_mutual: p3.fb_id, profile: p1)
--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -2,12 +2,12 @@
 
 FactoryGirl.define do
   factory :profile do
-    access_token ""
-    fb_id "MyString"
-    name "MyString"
-    birthday "2014-03-10"
-    gender "MyString"
-    relationship_status "MyString"
-    picture_url "MyString"
+    access_token { nil }
+    fb_id { [1000000000 .. 10000000000].sample.to_s }
+    name { Faker::Name.name }
+    birthday { [3000 .. 30000].sample.days.ago }
+    gender { ['male', 'female'].sample }
+    relationship_status { ['Single', 'In a Relationship', 'In an Open Relationship', 'Engaged', 'Married', 'It\'s Complicated', 'Widowed'].sample }
+    picture_url { "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(Faker::Internet.email)}" }
   end
 end
