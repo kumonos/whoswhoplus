7cebef4bfcfd90201d61710a030a3e4d83151808
tomoima525 <tomoima525@gmail.com>
2014-03-22 01:17:13 +0900

    友人一覧表示できるようにhome_controller,index.haml修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,18 @@ class HomeController < ApplicationController
     if @current_user
       # ログイン状態
       @friends = @current_user.api.get_object('/me/friends')
+      @friends_pic=@current_user.api.get_object('/me/friends','fields'=>'gender,birthday,picture')    
+      @friends_pic.each do |key|
+
+        #puts key["picture"]["data"]["url"].to_s
+         
+      end  
+      
     else
       # 非ログイン状態
       session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + ':3000/home/callback')
       @auth_url = session[:oauth].url_for_oauth_code(permissions: "read_stream")
+
       render 'intro'
     end
   end
@@ -27,22 +35,8 @@ class HomeController < ApplicationController
       redirect_to root_path
     #end
 
-		#begin
-		#	@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
-		#	@pic_data=@api.get_object("100003980373324","fields"=>"picture")
-		#	#userのプロフィール画像とれる
-		#	puts @api.get_object("100003980373324","fields"=>"picture")
-    #
-		#	#message一覧
-		#	puts @api.get_object("/me/statuses","fields"=>"message")
-		#rescue Exception=>ex
-		#	puts ex.message
-		#end
-		#puts @pic_data.class
-		#puts @pic_data['picture']['data']['url']
-		##parse がうまくいかない
-		##hash=JSON.parse @pic_data
-		##parsed=hash['picture']
+
   end
+
 end
 
