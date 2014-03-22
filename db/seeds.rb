7795796a142e50ebbe5d59b9baf3299921f39da1
youcune <i@youcune.com>
2014-03-22 21:53:40 +0900

    紹介画面を実装、テンプレートマスタを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+#require 'active_support'
+require 'faker'
 require 'factory_girl'
 Dir[Rails.root.join('spec/factories/*.rb')].each { |f| require f }
+
+# DBクリア
+AccessToken.delete_all
+Profile.delete_all
+Relation.delete_all
+Template.delete_all
 
 # Profile作成
 p1 = FactoryGirl.create(:profile)
@@ -16,3 +24,17 @@ p5 = FactoryGirl.create(:profile)
 FactoryGirl.create(:relation, friend_friend: p4.fb_id, friend_mutual: p2.fb_id, profile: p1)
 FactoryGirl.create(:relation, friend_friend: p5.fb_id, friend_mutual: p2.fb_id, profile: p1)
 FactoryGirl.create(:relation, friend_friend: p4.fb_id, friend_mutual: p3.fb_id, profile: p1)
+
+# テンプレート投入
+Template.create(
+  objective: '3人で飲みに行きたい',
+  body: '3人で飲みに行きませんか？'
+)
+Template.create(
+  objective: 'どんな子か知りたい',
+  body: '彼氏はいるんですか？'
+)
+Template.create(
+  objective: '連絡先を教えてほしい',
+  body: 'LINEおしえて！'
+)
