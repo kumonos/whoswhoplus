9e0ce5132ff88fab95ad49b43cf81078d7d9f686
youcune <i@youcune.com>
2014-03-24 08:01:59 +0900

    ダミーログイン画面を作成、サインアウトを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 SampleKoalaRailsApp::Application.routes.draw do
   root :to => 'home#index'
+
+  # サインイン時のコールバック画面
   get '/home/callback' => 'home#callback'
+
+  # サインアウト
+  get '/sign_out' => 'home#sign_out'
 
   # 紹介画面
   get '/relations/:user' => 'relations#index', as: :relations
 
   # メッセージ送信画面
   get '/relations/:user/via/:via' => 'relations#show', as: :relation
-  get 'home/callback' => 'home#callback'
 
   # 友人の友人一覧表示画面
   get '/friends/:fb_id' => 'friends#show', as: :friends
@@ -17,7 +21,9 @@ SampleKoalaRailsApp::Application.routes.draw do
 	collection {get "search"}
   end
 
+  # 開発環境のみのダミーログイン画面
+  if Rails.env.development?
+    get '/dummy_login' => 'home#dummy_form'
+    post '/dummy_login' => 'home#dummy_login'
+  end
 end
-
-
-
