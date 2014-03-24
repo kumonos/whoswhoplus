ba3bb30c3c7e135062f29c2f1ee87a9948d26b86
youcune <i@youcune.com>
2014-03-24 19:33:23 +0900

    コード修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ SampleKoalaRailsApp::Application.routes.draw do
   # サインアウト
   get '/sign_out' => 'home#sign_out'
 
+  # 友人の友人一覧表示画面
+  get '/friends/:fb_id' => 'friends#show', as: :friends
+
   # 紹介画面
   get '/relations/:user' => 'relations#index', as: :relations
 
   # メッセージ送信画面
   get '/relations/:user/via/:via' => 'relations#show', as: :relation
 
-  # 友人の友人一覧表示画面
-  get '/friends/:fb_id' => 'friends#show', as: :friends
-
   #profilesの扱いがよく解んなくなった時の残骸（削除してもOKかも）
   resources :profiles do
-	collection {get "search"}
+	collection { get "search" }
   end
 
   # 開発環境のみのダミーログイン画面
