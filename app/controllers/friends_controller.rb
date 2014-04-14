edafeb4b0223df37789e7d19ad139751973a2aa1
tomoima525 <tomoima525@gmail.com>
2014-04-15 01:35:36 +0900

    友人一覧画面にて画像ロードを初回のみに実装

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,34 +1,31 @@
 class FriendsController < ApplicationController
-
+@@check_flg = "false"
+	   
 	# 検索ページ
 	def show
 		#ユーザーの友人のfb_idよりトークンを取得
 		@profile=Profile.getUser(params[:fb_id])
 
-		#TODO:毎回インサートは実施しないようにする
-		#TODO:ajaxな表示にする
-
+        if @@check_flg == "false"
 		#ユーザーの友人の友人情報をprofilesに格納
 		@friends_friends=@profile.api.get_object('/me/friends','fields'=>'name,gender,picture,relationship_status')
 		Profile.insert(@friends_friends)
 		#「ユーザーの友人」と「友人の友人」のfb_idをrelationsに登録する
 		Relation.insert(params[:fb_id],@friends_friends)
-
-	    @search_form =  SearchForm.new params[:search_form]
-	
-
+		@@check_flg = "true"
+	    end
+		@search_form =  SearchForm.new params[:search_form]
 	    @results=Profile.search(:gender => @search_form.gender,:relationship_status =>@search_form.relationship_status)
-
 
 		render 'friends' #viewのhamlの名前
 	end
 
 	
 	# 検索結果ページ
-	def search
-		@results = Profile.search(params[:gender])
+	def search_result
+
+
 	
-		render 'friends'
 	end
 
   private
