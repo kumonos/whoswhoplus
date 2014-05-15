9905d3c65d94c8ec7b94fa70d648d8264c1a7605
tomoima525 <tomoima525@gmail.com>
2014-05-15 09:40:40 +0900

    年齢バリデーション追加、テストコード追加

--- app/models/search_form.rb
+++ app/models/search_form.rb
@@ -5,6 +5,11 @@ class SearchForm
   validates :age_min, :numericality => {:greater_than_or_equal_to =>0,:message=>'年齢は0以上にしてください'}
   # {:less_than_or_equal_to => :age_max}にするとArgumentError - comparison of Float with String failedでエラー発生
   validates :age_max, :numericality => {:greater_than_or_equal_to =>0,:message=>'年齢は0以上にしてください'}
+  validate :age_check
+
+  def age_check
+    errors.add(:age_min ,'最小年齢は最大年齢以下にしてください') if age_min > age_max
+  end
 
 
   def initialize(params)
--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe FriendsController do
-
+  describe 'GET #show' do
+  	it '最小年齢が0以下の場合は[年齢は0以上にしてください]'
+  	it '最大年齢が0以下[年齢は0以上にしてください]'
+  	it '最小年齢が最大年齢より大きい[最小年齢は最大年齢以上にしてください]'
+  end
 end
