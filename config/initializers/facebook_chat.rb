825fa32ceb8d9fa726c3aae906b48e95dfe1c02b
youcune <you@nakanishi.email>
2014-05-26 08:42:02 +0900

    coses #71

--- config/initializers/facebook_chat.rb
+++ config/initializers/facebook_chat.rb
@@ -1,6 +1,5 @@
 require 'facebook_chat'
 
 FacebookChat::Client.configure do |config|
-  config.api_key = CLIENT_TOKEN # your facebook application's api key
-  config.host = 'chat.facebook.com' # you can omit this line. Default host value is 'chat.facebook.com'
+  config.api_key = defined?(CLIENT_TOKEN) ? CLIENT_TOKEN : 'DUMMY'
 end
