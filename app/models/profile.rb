3d8be57f0abe308767f28bac993766ab03d1b3f5
tomoima525 <tomoima525@gmail.com>
2014-08-06 09:07:05 +0900

    共通知り合い多い上位20人のうちトークンがない(招待できる)友人を表示

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -188,8 +188,8 @@ class Profile < ActiveRecord::Base
     friends_id = Relation.where(fb_id_from: profiles.friends_of_from_user.has_token.pluck(:fb_id))
                       .group(:fb_id_to)
                       .order('count_fb_id_to desc')
-                      .count(:fb_id_to).keys.first(100)
-    friends = Profile.where(fb_id: friends_id.map{ |f| f})
+                      .count(:fb_id_to).keys.first(20)
+    friends = Profile.where(fb_id: friends_id.map{ |f| f}).has_no_token
               
     return friends
   end
